7305c4790a62a8c2e82a7a1bfd25ed3f2108b383
Ian <imatlak03@gmail.com>
2021-03-31 01:00:27 -0400

    Covid data displaying

--- src/components/Map.js
+++ src/components/Map.js
@@ -10,12 +10,11 @@ import USstatesPoly from '../data/us-states_poly.json';
 import randomPointsOnPolygon from 'random-points-on-polygon';
 import * as turf from '@turf/turf';
 import states from 'us-state-codes';
-
 // Viewport settings
 const INITIAL_VIEW_STATE = {
-  longitude: -122.466233,
-  latitude: 37.684638,
-  zoom: 13,
+  longitude: -87.6500523,
+  latitude: 41.850033,
+  zoom: 4,
   pitch: 0,
   bearing: 0,
 };
@@ -24,101 +23,128 @@ const INITIAL_VIEW_STATE = {
 const MAPBOX_TOKEN = process.env.REACT_APP_API_KEY;
 export function Map() {
   console.log('Map');
-  const { vaccineData, covidData, clickFunc } = useContext(CovidContext);
+  const { vaccineData, covidData } = useContext(CovidContext);
   console.log(vaccineData);
   console.log(covidData);
+
   let allVaccinePoints = [];
 
   vaccineData.map((j) => {
-    USstatesPoly.features.map((i) => {
-      if (j.state === i.properties.name) {
-        if (i.geometry.type === 'Polygon') {
-          let polygon = turf.polygon(i.geometry.coordinates);
-          var points = randomPointsOnPolygon(
-            j.data.doses_admin / 10000,
-            polygon
-          );
-        } else if (i.geometry.type === 'MultiPolygon') {
-          let multipolygon = turf.multiPolygon(i.geometry.coordinates);
-          var points = randomPointsOnPolygon(
-            j.data.doses_admin / 10000,
-            multipolygon
-          );
-        } else {
-          console.log('NOT POLY OR MULTI');
+    let points = 0;
+
+    if (j.data.doses_admin !== 0) {
+      USstatesPoly.features.map((i) => {
+        if (j.state === i.properties.name) {
+          if (i.geometry.type === 'Polygon') {
+            let polygon = turf.polygon(i.geometry.coordinates);
+            points = randomPointsOnPolygon(j.data.doses_admin / 10000, polygon);
+          } else if (i.geometry.type === 'MultiPolygon') {
+            let multipolygon = turf.multiPolygon(i.geometry.coordinates);
+            points = randomPointsOnPolygon(
+              j.data.doses_admin / 10000,
+              multipolygon
+            );
+          } else {
+            console.log('NOT POLY OR MULTI');
+          }
+          j.points = points;
+          allVaccinePoints.push(...points);
         }
-        j.points = points;
-        allVaccinePoints.push(...points);
-        // console.log(allVaccinePoints);
-      }
-    });
+      });
+    } else {
+      console.log('ERROR DATA', j.state);
+    }
   });
 
-  let allCoivdPoints = [];
+  let allCovidPoints = [];
 
   covidData.map((j) => {
-    let alphaCode = j.state;
-    j.state = states.getStateNameByStateCode(alphaCode);
-    console.log(j.state, states.getStateNameByStateCode(alphaCode));
-  });
-
-  covidData.map((j) => {
-    USstatesPoly.features.map((i) => {
-      if (j.state === i.properties.name) {
-        if (i.geometry.type === 'Polygon') {
-          let polygon = turf.polygon(i.geometry.coordinates);
-          var points = randomPointsOnPolygon(j.positive / 10000, polygon);
-        } else if (i.geometry.type === 'MultiPolygon') {
-          let multipolygon = turf.multiPolygon(i.geometry.coordinates);
-          var points = randomPointsOnPolygon(j.positive / 10000, multipolygon);
-        } else {
-          console.log('NOT POLY OR MULTI');
+    // If state data is not 0 then execute code
+    if (j.positive !== 0) {
+      // *API returns data in abbreviation form*
+      // Convert State Code from the API to State Name
+      let alphaCode = j.state;
+      let state = states.getStateNameByStateCode(alphaCode);
+      // Maps through polygon list of each state
+      USstatesPoly.features.map((i) => {
+        let points = 0;
+        // if covidData state name equals polygon data state name
+        if (state === i.properties.name) {
+          if (i.geometry.type === 'Polygon') {
+            let polygon = turf.polygon(i.geometry.coordinates);
+            points = randomPointsOnPolygon(j.positive / 10000, polygon);
+          } else if (i.geometry.type === 'MultiPolygon') {
+            let multipolygon = turf.multiPolygon(i.geometry.coordinates);
+            points = randomPointsOnPolygon(j.positive / 10000, multipolygon);
+          } else {
+            console.log('NOT POLY OR MULTI');
+          }
+          console.log(points);
+          allCovidPoints.push(...points);
         }
-        allCoivdPoints.push(...points);
-        console.log(allCoivdPoints, j.state);
-      }
-    });
+      });
+    }
   });
 
   // / 1000
-  const layer = new ScatterplotLayer({
-    id: 'scatterplot-vaccine-layer',
-    data: allVaccinePoints,
-    pickable: true,
-    opacity: 0.8,
-    stroked: true,
-    filled: true,
-    radiusScale: 1,
-    radiusMinPixels: 2,
-    radiusMaxPixels: 10,
-    lineWidthMinPixels: 1,
-    getPosition: (d) => d.geometry.coordinates,
-    getFillColor: (d) => [0, 0, 255],
-    getLineColor: (d) => [0, 0, 0],
-  });
+  // const layer = new ScatterplotLayer({
+  //   id: 'scatterplot-vaccine-layer',
+  //   data: allVaccinePoints,
+  //   pickable: true,
+  //   opacity: 0.8,
+  //   stroked: true,
+  //   filled: true,
+  //   radiusScale: 1,
+  //   radiusMinPixels: 2,
+  //   radiusMaxPixels: 10,
+  //   lineWidthMinPixels: 1,
+  //   getPosition: (d) => d.geometry.coordinates,
+  //   getFillColor: (d) => [0, 0, 255],
+  //   getLineColor: (d) => [0, 0, 0],
+  // });
+  // console.log('vaccine', allVaccinePoints);
 
-  const covid_Layer = new ScatterplotLayer({
-    id: 'scatterplot-covid-layer',
-    data: allCoivdPoints,
-    pickable: true,
-    opacity: 0.8,
-    stroked: true,
-    filled: true,
-    radiusScale: 1,
-    radiusMinPixels: 2,
-    radiusMaxPixels: 10,
-    lineWidthMinPixels: 1,
-    getPosition: (d) => d.geometry.coordinates,
-    getFillColor: (d) => [255, 0, 0],
-    getLineColor: (d) => [0, 0, 0],
-  });
+  const layers = [
+    new ScatterplotLayer({
+      id: 'scatterplot-covid-layer',
+      data: allCovidPoints,
+      pickable: true,
+      opacity: 0.8,
+      stroked: true,
+      filled: true,
+      radiusScale: 1,
+      radiusMinPixels: 2,
+      radiusMaxPixels: 10,
+      lineWidthMinPixels: 1,
+      getPosition: (d) => d.geometry.coordinates,
+      getFillColor: (d) => [255, 0, 0],
+      getLineColor: (d) => [0, 0, 0],
+    }),
+    new ScatterplotLayer({
+      id: 'scatterplot-vaccine-layer',
+      data: allVaccinePoints,
+      pickable: true,
+      opacity: 0.8,
+      stroked: true,
+      filled: true,
+      radiusScale: 1,
+      radiusMinPixels: 2,
+      radiusMaxPixels: 10,
+      lineWidthMinPixels: 1,
+      getPosition: (d) => d.geometry.coordinates,
+      getFillColor: (d) => [0, 0, 255],
+      getLineColor: (d) => [0, 0, 0],
+    }),
+  ];
+  console.log('covid', allCovidPoints);
+  console.log('vaccine', allVaccinePoints);
 
   return (
     <div className='map'>
       <DeckGL
         initialViewState={INITIAL_VIEW_STATE}
         controller={true}
-        layers={[covid_Layer]}
+        layers={layers}
       >
         <StaticMap mapboxApiAccessToken={MAPBOX_TOKEN} />
       </DeckGL>
